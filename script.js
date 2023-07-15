2647c11260aec6c6cf29aadd7ecd713fe5b81b62
S Bhuse <122111054+Shubhangibhuse@users.noreply.github.com>
2023-07-14 20:45:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let outputScreen = document.getElementById("output-screen");
+
+function display(num){
+    outputScreen.value += num;
+}
+
+function Calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err){
+        alert("Invalid")
+    }
+}
+
+function Clear(){
+    outputScreen.value = "";
+}
+function del(){
+    outputScreen.value= outputScreen.value.slice(0,-1);
+}
